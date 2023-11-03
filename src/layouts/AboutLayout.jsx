c19eef30910d60cea005681424eac9a4b5e31a39
beefcarhorn <113559659+beefcarhorn@users.noreply.github.com>
2023-11-03 17:25:27 -0400

    about-page-skeleton

---
+++ src/layouts/AboutLayout.jsx
@@ -0,0 +1,18 @@
+import ContactUs from "@/components/ContactUs"
+import Footer from "@/components/Footer"
+import { Carousel } from '@mantine/carousel';
+
+export default function AboutLayout() {
+  return (
+    <div className="flex flex-col items-center text-center">
+        <h1 className="text-6xl font-bold mb-6">What is IEEE?</h1>
+        <h2 className="text-xl w-1/3 mb-12">IEEE is the world's largest technical professional organization, dedicated to advancing technology for the benefit of humanity. IEEE publishes nearly one third of technical papers and research all across the realm of electronics.</h2>
+        <h1 className="text-6xl font-bold mb-6">IEEE at Purdue</h1>
+        <h2 className="text-xl w-1/3 mb-12">The Purdue IEEE Student Branch actually pre-dates the international IEEE! We were founded as a student chapter of the American Institute of Electrical Engineers (AIEE) in 1903. When the AIEE merged with other organizations in 1963 to create the IEEE, we were reborn as Purdue IEEE. Ever since that, we have been committeed to exploring technology and introducing Purdue students to opportunities to apply their knowledge in practical applications.</h2>
+        <ContactUs />
+        <Carousel withIndicators height={200}></Carousel>
+        <Footer />
+    </div>
+  )
+}
+
--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,11 +1,11 @@
 import CommitteeLayout from "./CommitteeLayout";
-import OfficersLayout from "./OfficersLayout";
+import AboutLayout from "./AboutLayout";
 
 export default function LandingLayout() {
   return (
     <div className="px-20">
       <CommitteeLayout />
-      <OfficersLayout />
+      <AboutLayout />
     </div>
   )
 }
